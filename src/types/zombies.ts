c4c530cd6eb2419c8e16ba55cb2cfad45e2623af
Niklas Mårdby <niklas.mardby@gmail.com>
2025-01-17 13:28:38 +0100

    Updated zombie type

--- src/types/zombies.ts
+++ src/types/zombies.ts
@@ -1,1 +1,11 @@
 // här är alla typer som har med mina zombier att göra
+
+import { DisneyCharacter } from "./disneyAPI";
+
+export type ZombieSpeed = "slow" | "normal" | "fast";
+
+export type Zombie = {
+	name: string;
+	speed: ZombieSpeed;
+	favouriteDisneyCharacter: DisneyCharacter[];
+};
