301c845d2a92ecba8b075f23e57f43707dc3a555
Niklas Mårdby <niklas.mardby@gmail.com>
2025-01-20 08:32:50 +0100

    Updated CharacterList component

--- src/components/CharacterList/CharacterList.ts
+++ src/components/CharacterList/CharacterList.ts
@@ -1,1 +1,10 @@
 import "./CharacterList.scss";
+
+const CharacterList = () => {
+	const div = document.createElement("div");
+	div.innerHTML = `test`;
+	div.classList.add("CharacterList");
+	return div;
+};
+
+export default CharacterList;
