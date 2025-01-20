015e03b5154003479d401663ea161ef6b7a1a86f
Niklas Mårdby <niklas.mardby@gmail.com>
2025-01-20 08:25:32 +0100

    Started workingn on global state functionality

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,1 +1,18 @@
-// här hanterar jag mitt globala state
+// här hanterar vi vårt globala state
+
+import { Zombie } from "../types/zombies";
+
+export const zombieState: Zombie[] = [
+	{ name: "Bob", speed: "normal", favouriteDisneyCharacter: null },
+	{ name: "Stina", speed: "fast", favouriteDisneyCharacter: null },
+	{ name: "Kim", speed: "slow", favouriteDisneyCharacter: null },
+];
+
+export const addZombie = (z: Zombie) => {
+	zombieState.push(z);
+};
+
+// export const addFavouriteCharacter = (
+// 	characterName: string,
+// 	zombieName: string
+// ) => {};
